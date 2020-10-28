737ccb7e5d79cf945b6b57b9cbe863dca9f56e40
Liam Cox <liamcox@Liams-MacBook-Pro.local>
2020-10-28 17:35:23 -0500

    created buttons and navigation

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,30 +3,34 @@ import { View, Text, StyleSheet, FlatList} from "react-native"
 
 const ListScreen = () => {
     let friends = [
-        {name: 'Friend #1', key:"1"},
-        {name: 'Friend #2', key:"2"},
-        {name: 'Friend #3', key:"3"},
-        {name: 'Friend #4', key:"4"},
-        {name: 'Friend #5', key:"5"},
-        {name: 'Friend #6', key:"6"},
-        {name: 'Friend #7', key:"7"},
-        {name: 'Friend #8', key:"8"},
-        {name: 'Friend #9', key:"9"},
-        {name: 'Friend #10', key:"10"},
+        {name: 'Friend #1', age:22, key:"1"},
+        {name: 'Friend #2', age:34, key:"2"},
+        {name: 'Friend #3', age:25, key:"3"},
+        {name: 'Friend #4', age:43, key:"4"},
+        {name: 'Friend #5', age:21, key:"5"},
+        {name: 'Friend #6', age:22, key:"6"},
+        {name: 'Friend #7', age:33, key:"7"},
+        {name: 'Friend #8', age:45, key:"8"},
+        {name: 'Friend #9', age:55, key:"9"},
+        {name: 'Friend #10', age:47, key:"10"},
     ]
     
 
     return  <FlatList 
                 data={friends}
                 renderItem={({ item }) => {
-                    return <Text style={styles.textStyle}>{item.name}</Text>
+                    return (
+                    <View>
+                        <Text style={styles.textStyle}>{item.name} - Age: {item.age}</Text>
+                    </View>
+                    )
                 }} 
                     />
 }
 
 const styles = StyleSheet.create({
     textStyle : {
-        marginVertical: 10
+        marginVertical: 50
     }
 })
 
